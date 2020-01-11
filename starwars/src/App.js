5cc2b7298058bd7318ab05d6713dce76a1f76d1a
rdowd <rjarrettdowd@yahoo.com>
2020-01-10 21:28:49 -0500

    finished card component and have all data rendering to the page.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
-import './App.css';
+import Card from "./Card";
+import styled from "styled-components"
+// import './App.css';
+
+const AppH1 = styled.h1`
+  text-align: center;
+`;
+
+const FlexDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 900px
+  margin: 0px auto;
+  
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +26,7 @@ const App = () => {
     axios
       .get(`https://swapi.co/api/people/`)
       .then(res => {
-        setApiData(res);
+        setApiData(res.data.results);
       })
       .catch(error =>{
         console.log("Api data Error:", error);
@@ -19,6 +34,7 @@ const App = () => {
   },[]);
 
   console.log("Api data:", apiData);
+  
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -26,8 +42,21 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <AppH1 className="Header">React Wars</AppH1>
+      <FlexDiv>
+        {apiData.map((x, index)=>{
+          return(
+            <div key={index}>
+              <Card  name={x.name} height={x.height} mass={x.mass}/>
+            </div>  
+          );
+          
+        })}
+        
+      </FlexDiv>
+    
     </div>
+    
   );
 }
 
--- starwars/src/Card.js
+++ starwars/src/Card.js
@@ -1,9 +1,27 @@
 import React from "react";
+import styled from "styled-components"
 
-export default function Card(){
+const CardDiv = styled.div`
+    width: 200px;
+    height: 200px;
+    background: rgb(204, 163, 0, 0.7);
+    border: 2px solid black;
+    border-radius 10px;
+    margin: 5px;
+    text-align: center;
+    
+    
+
+`;
+
+
+
+export default function Card(props){
     return(
-        <div>
-            
-        </div>
+        <CardDiv>    
+            <h2>{props.name}</h2>
+            <p>Height: {props.height}</p>
+            <p>Mass: {props.mass}</p>     
+        </CardDiv>
     );
 }
